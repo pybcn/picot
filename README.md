0b0aed7779ca0e2d3e8a3a03bb3033c462132f7a
Ignasi Fosch <natx@y10k.ws>
2018-12-14 03:05:36 +0100

    First Picot MVP
    In this first MVP, picot, as a tool will only print the entries' titles for the
    feed used as argument.
    Since this is not requiring a lot of logic in this level, no tests have been
    added so far.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # picot
 
 Picot is a library to read, select and extract information from RSS feeds.
+
+# Usage
+
+Picot can be invoked as a command line tool to get the titles of the entries in an RSS:
+```
+$ pipenv run bin/picot <RSS URL>
+```
---
+++ picot/feed.py
@@ -0,0 +1,14 @@
+import feedparser
+
+class Feed(object):
+    def __init__(self, url):
+        self._feed = feedparser.parse(url)
+
+    def __iter__(self):
+        return iter(self._feed.entries)
+
+    def __repr__(self):
+        return "{} ({})".format(
+            self._feed.feed.title,
+            self._feed.feed.link
+        )
