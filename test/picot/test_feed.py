3cae06bd523a3bac93d976f1334069bb708f5ed9
Ignasi Fosch <natx@y10k.ws>
2018-12-19 09:58:48 +0100

    Add tests for picot.Feed

---
+++ test/picot/test_feed.py
@@ -0,0 +1,61 @@
+import pytest
+
+import feedparser
+
+import picot.feed
+
+class MockFeedProperty(object):
+    entries = []
+    link = ''
+    title = ''
+
+class MockFeed(object):
+    def __init__(self, url, entries=None):
+        if entries is None:
+            entries = []
+        self.entries = entries
+        self.feed = MockFeedProperty()
+        self.feed.link = url
+        self.feed.title = 'Feed at {}'.format(url)
+
+@pytest.mark.parametrize(
+    'expected_entries',
+    [
+        ([]),
+        ([
+            {
+                'title': 'Some entry',
+            },
+        ]),
+        ([
+            {
+                'title': 'Some entry',
+            },
+            {
+                'title': 'Some other entry',
+            },
+        ]),
+    ],
+    ids=[
+        'No entries',
+        'Single entry',
+        'Multiple entries',
+    ],
+)
+def test_feed(expected_entries, monkeypatch):
+    def mocked_parse(url):
+        return MockFeed(
+            url,
+            entries=expected_entries,
+        )
+    monkeypatch.setattr(feedparser, 'parse', mocked_parse)
+    feed = picot.feed.Feed('https://some.url')
+    assert(len(feed) == len(expected_entries))
+    assert(repr(feed) == 'Feed at https://some.url (https://some.url)')
+    for entry in feed:
+        assert(len(entry) == len(expected_entries[0]))
+        for expected_entry in expected_entries:
+            for key in entry:
+                if entry[key] == expected_entry[key]:
+                    assert(entry[key] == expected_entry[key])
+                    break
